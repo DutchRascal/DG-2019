aa0964892a30f6f3e232f0bdabc9945413a76a79
DutchRascal <DutchRascal@fritz.box>
2019-10-30 16:56:42 +0100

    DG-010

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,13 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveInput;
     private Rigidbody2D theRB;
     private Transform gunArm;
+    private Camera theCam;
 
     void Start()
     {
         theRB = FindObjectOfType<Rigidbody2D>();
         gunArm = GameObject.Find("Gun Hand").GetComponent<Transform>();
+        theCam = Camera.main;
     }
 
     void Update()
@@ -21,10 +23,21 @@ public class PlayerController : MonoBehaviour
         moveInput.y = Input.GetAxisRaw("Vertical");
         theRB.velocity = moveInput * moveSpeed;
         Vector3 mousePosition = Input.mousePosition;
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+        if (mousePosition.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
         //rotate arm
         Vector2 offset = new Vector2(mousePosition.x - screenPoint.x, mousePosition.y - screenPoint.y);
         float angel = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
         gunArm.rotation = Quaternion.Euler(0f, 0f, angel);
     }
 }
