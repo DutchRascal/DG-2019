22706ccd6cec32c4c51ad1516d8450c1604ede12
DutchRascal <DutchRascal@fritz.box>
2019-10-31 16:55:56 +0100

    DG-016

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -4,6 +4,7 @@ public class PlayerBullet : MonoBehaviour
 {
     public float speed = 7.5f;
     private Rigidbody2D theRB;
+    public GameObject impactEffect;
 
     void Start()
     {
@@ -16,6 +17,12 @@ public class PlayerBullet : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
     {
         Destroy(gameObject);
     }
