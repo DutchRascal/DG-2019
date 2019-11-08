1443d356e2803bd688c01c9d276f3cfd53f3661f
DutchRascal <DutchRascal@fritz.box>
2019-11-08 16:46:50 +0100

    DG-027

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -16,6 +16,14 @@ public class PlayerHealthController : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
+        UpdateUIElements();
+    }
+
+    private void UpdateUIElements()
+    {
+        UIController.instance.healthSlider.maxValue = maxHealth;
+        UIController.instance.healthSlider.value = currentHealth;
+        UIController.instance.healthText.text = currentHealth + " / " + maxHealth;
     }
 
     void Update()
@@ -26,6 +34,7 @@ public class PlayerHealthController : MonoBehaviour
     public void DamagePlayer()
     {
         currentHealth--;
+        UpdateUIElements();
         if (currentHealth <= 0)
         {
             PlayerController.instance.gameObject.SetActive(false);
