24e4dfab3547f9c74526b78c97471f16f001d919
DutchRascal <DutchRascal@fritz.box>
2019-11-17 12:46:35 +0100

    DG-036

--- Assets/Scripts/Breakables.cs
+++ Assets/Scripts/Breakables.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Breakables : MonoBehaviour
 {
+
+    public GameObject[] brokenPieces;
+    private GameObject brokenPiece;
+    private int maxPieces = 5;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player" && PlayerController.instance.isDashing)
         {
             Destroy(gameObject);
+            for (int i = 0; i < Random.Range(1, maxPieces); i++)
+            {
+                brokenPiece = brokenPieces[Random.Range(0, brokenPieces.Length)];
+                Instantiate(brokenPiece, transform.position, transform.rotation);
+            }
         }
     }
 }
