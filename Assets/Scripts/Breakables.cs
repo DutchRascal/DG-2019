3d15a522dcdac2ae56e085ec026ace39f638fa92
DutchRascal <DutchRascal@fritz.box>
2019-11-18 16:50:48 +0100

    DG-041

--- Assets/Scripts/Breakables.cs
+++ Assets/Scripts/Breakables.cs
@@ -5,19 +5,37 @@ using UnityEngine;
 public class Breakables : MonoBehaviour
 {
 
-    public GameObject[] brokenPieces;
-    private GameObject brokenPiece;
+    private GameObject
+            brokenPiece,
+            dropItem;
     private int maxPieces = 5;
+
+    public bool shouldDropItem;
+    public GameObject[]
+            brokenPieces,
+            itemsToDrop;
+    public float itemDropPercentage;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player" && PlayerController.instance.isDashing)
         {
             Destroy(gameObject);
+            //show broken pieces
             for (int i = 0; i < Random.Range(1, maxPieces); i++)
             {
                 brokenPiece = brokenPieces[Random.Range(0, brokenPieces.Length)];
                 Instantiate(brokenPiece, transform.position, transform.rotation);
+            }
+            //drop items
+            if (shouldDropItem)
+            {
+                float dropChance = Random.Range(0f, 100f);
+                if (dropChance < itemDropPercentage)
+                {
+                    dropItem = itemsToDrop[Random.Range(0, itemsToDrop.Length)];
+                    Instantiate(dropItem, transform.position, transform.rotation);
+                }
             }
         }
     }
--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class HealthPickup : MonoBehaviour
 {
+    private float waitToBeCollected = .5f;
 
     public int healAmount = 1;
 
+    private void Update()
+    {
+        if (waitToBeCollected > 0)
+        {
+            waitToBeCollected -= Time.deltaTime;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && waitToBeCollected <= 0)
         {
             PlayerHealthController.instance.healPlayer(healAmount);
             Destroy(gameObject);
