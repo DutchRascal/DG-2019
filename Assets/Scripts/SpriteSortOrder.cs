e63e72019471eae71b2a3c1f78804aeca1a8a356
DutchRascal <DutchRascal@fritz.box>
2019-11-18 08:13:42 +0100

    DG-039

---
+++ Assets/Scripts/SpriteSortOrder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSortOrder : MonoBehaviour
+{
+
+    private SpriteRenderer theSR;
+
+    void Start()
+    {
+        theSR = GetComponent<SpriteRenderer>();
+        theSR.sortingOrder = Mathf.RoundToInt(-10f * transform.position.y);
+    }
+
+    void Update()
+    {
+
+    }
+}
