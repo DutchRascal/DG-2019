7e059ef565393dca17971ccad595d560f33151ef
DutchRascal <DutchRascal@fritz.box>
2019-11-22 16:38:31 +0100

    DG-045

--- Assets/Scripts/Breakables.cs
+++ Assets/Scripts/Breakables.cs
@@ -18,7 +18,7 @@ public class Breakables : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player" && PlayerController.instance.isDashing)
+        if ((other.tag == "Player" && PlayerController.instance.isDashing) || other.tag == "PlayerBullet")
         {
             Destroy(gameObject);
             AudioManager.instance.PlaySFX(0);
