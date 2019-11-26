64497adf04cc69aba7a8d4da2e48957115d69157
DutchRascal <DutchRascal@fritz.box>
2019-11-26 16:35:19 +0100

    DG-051

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public bool closeWhenEnterred;
+    public GameObject[] doors;
+
+    private void Start()
+    {
+        // HandleDoors();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
             CameraController.instance.ChangeTarget(transform);
+            HandleDoors();
+        }
+    }
+
+    private void HandleDoors()
+    {
+        print("Handle Door");
+        if (closeWhenEnterred)
+        {
+            foreach (GameObject door in doors)
+            {
+                print("Loop...");
+                door.SetActive(true);
+            }
         }
     }
 }
