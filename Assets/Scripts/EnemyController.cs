60e3ef040fa2d5f97a6d9190e54c0ca35cdbbe7e
DutchRascal <DutchRascal@fritz.box>
2019-12-02 13:04:04 +0100

    DG-053

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,7 +6,7 @@ public class EnemyController : MonoBehaviour
 {
     private Rigidbody2D theRB;
     private Vector3 moveDirection;
-    private SpriteRenderer enemyBody;
+    public SpriteRenderer enemyBody;
     private Animator animator;
     private float fireCounter;
     private bool isEnemyOnScreen;
@@ -28,7 +28,6 @@ public class EnemyController : MonoBehaviour
     {
         theRB = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        enemyBody = GameObject.Find("Body").GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -59,6 +58,7 @@ public class EnemyController : MonoBehaviour
 
     private void CheckIfEnemyIsOnScreen()
     {
+        print(enemyBody.isVisible);
         isEnemyOnScreen = enemyBody.isVisible ? true : false;
     }
 
@@ -78,6 +78,7 @@ public class EnemyController : MonoBehaviour
 
     private void EnemyMoving()
     {
+        print("Enemy Moving");
         if (Vector3.Distance(enemyPosition, playerPosition) < rangeToChasePlayer)
         {
             moveDirection = playerPosition - enemyPosition;
