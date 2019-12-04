cc2a37004b24545bebeba499ba85f16127563b44
DutchRascal <DutchRascal@fritz.box>
2019-12-04 15:36:00 +0100

    DG-055

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,17 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 public class LevelExit : MonoBehaviour
 {
-    public string levelToLoad;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(levelToLoad);
+
+            AudioManager.instance.PlayLevelWin();
+            StartCoroutine(LevelManager.instance.levelEnd());
         }
     }
 }
