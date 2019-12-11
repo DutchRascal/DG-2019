1a8e5ecf745a8ab7a99b6aabaf28d1808ba5be58
DutchRascal <DutchRascal@fritz.box>
2019-12-11 15:28:50 +0100

    DG-065

--- Assets/Scripts/LevelGenrator.cs
+++ Assets/Scripts/LevelGenrator.cs
@@ -2,17 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGenrator : MonoBehaviour
+public class LevelGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject layoutyRoom;
+    public int distanceToEnd;
+    public Color
+            startColor,
+            endColor;
+    public Transform generatorPoint;
+    public enum Direction
+    {
+        up,
+        right,
+        down,
+        left
+    }
+    public Direction selectedDirection;
+    public float
+        xOffSet = 18f,
+        yOffSet = 10f;
+
     void Start()
     {
-        
+        Instantiate(layoutyRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+        selectedDirection = (Direction)Random.Range(0, 4);
+        MoveGenerationPoint();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void MoveGenerationPoint()
+    {
+        switch (selectedDirection)
+        {
+            case Direction.up:
+                generatorPoint.position += new Vector3(0f, yOffSet, 0f);
+                break;
+            case Direction.right:
+                generatorPoint.position += new Vector3(xOffSet, 0f, 0f);
+                break;
+            case Direction.down:
+                generatorPoint.position += new Vector3(0f, -yOffSet, 0f);
+                break;
+            case Direction.left:
+                generatorPoint.position += new Vector3(-xOffSet, 0f, 0f);
+                break;
+        }
     }
 }
