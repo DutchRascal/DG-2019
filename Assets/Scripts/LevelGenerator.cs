ac650ed8ec055de92206b8241c6848468fb5bed7
DutchRascal <DutchRascal@fritz.box>
2019-12-12 10:16:37 +0100

    DG-070

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,18 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LevelGenrator : MonoBehaviour
+public class LevelGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject layoutyRoom;
+    public int distanceToEnd;
+    public Color
+            startColor,
+            endColor;
+    public Transform generatorPoint;
+    public enum Direction
+    {
+        up,
+        right,
+        down,
+        left
+    }
+    public Direction selectedDirection;
+    public float
+        xOffSet = 18f,
+        yOffSet = 10f;
+    public LayerMask whatIsRoom;
+    public RoomPrefabs roomPrefabs;
+
+    private GameObject endRoom;
+    private List<GameObject> layoutRoomObjects = new List<GameObject>();
+
     void Start()
     {
-        
+        Instantiate(layoutyRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
+        selectedDirection = (Direction)Random.Range(0, 4);
+        MoveGenerationPoint();
+        for (int i = 0; i < distanceToEnd; i++)
+        {
+            GameObject newRoom = Instantiate(layoutyRoom, generatorPoint.position, generatorPoint.rotation);
+            layoutRoomObjects.Add(newRoom);
+            if (i + 1 == distanceToEnd)
+            {
+                newRoom.GetComponent<SpriteRenderer>().color = endColor;
+                layoutRoomObjects.RemoveAt(layoutRoomObjects.Count - 1);
+                endRoom = newRoom;
+            }
+            selectedDirection = (Direction)Random.Range(0, 4);
+            MoveGenerationPoint();
+            while (Physics2D.OverlapCircle(generatorPoint.position, 0.2f, whatIsRoom))
+            {
+                print("Duplicate");
+                MoveGenerationPoint();
+            }
+        }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void MoveGenerationPoint()
+    {
+        switch (selectedDirection)
+        {
+            case Direction.up:
+                generatorPoint.position += new Vector3(0f, yOffSet, 0f);
+                break;
+            case Direction.right:
+                generatorPoint.position += new Vector3(xOffSet, 0f, 0f);
+                break;
+            case Direction.down:
+                generatorPoint.position += new Vector3(0f, -yOffSet, 0f);
+                break;
+            case Direction.left:
+                generatorPoint.position += new Vector3(-xOffSet, 0f, 0f);
+                break;
+        }
+    }
+}
+
+[System.Serializable]
+public class RoomPrefabs
+{
+    public GameObject
+            singleUp, singleDown, singleRight, singleLeft,
+            doubleUpDown, doubleLeftRight, doubleUpRight, doubleRightDown, doubleDownLeft, doubleLeftUp,
+            tripleUpRighDown, tripleRightDownLeft, tripleDownLeftUp, tripleLeftUpRight,
+            fourway;
 }
