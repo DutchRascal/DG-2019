60df08234c03db5618bb4512c1471f5608103abb
DutchRascal <DutchRascal@fritz.box>
2020-01-05 11:59:15 +0100

    DG-100

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -5,25 +5,33 @@ using UnityEngine.UI;
 
 public class Chest : MonoBehaviour
 {
-    public static Chest instance;
-
     public GameObject
             coinsToShow,
             openChest;
     public int numberOfCoins;
+    public GunPickup[] potentialGuns;
+    public Transform spawnPoint;
 
     [SerializeField] private float maxCoins = 25;
+    public float scaleSpeed = 10f;
 
-    private void Awake()
-    {
-        instance = this;
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            numberOfCoins = (int)Random.Range(0, maxCoins);
-            print(numberOfCoins);
+            int randomNumber = Random.Range(0, 1000) % 2;
+            if (randomNumber == 0)
+            {
+                numberOfCoins = (int)Random.Range(0, maxCoins);
+                print(numberOfCoins);
+            }
+            else
+            {
+                int gunSelect = Random.Range(0, potentialGuns.Length);
+                Vector3 gunPosition = new Vector3(transform.position.x + Random.Range(1, 1), transform.position.y + Random.Range(1, 1), transform.position.z);
+                // Instantiate(potentialGuns[gunSelect], gunPosition, transform.rotation);
+                Instantiate(potentialGuns[gunSelect], spawnPoint.position, spawnPoint.rotation);
+            }
             Instantiate(openChest, transform.position, transform.rotation);
             for (int i = 0; i < numberOfCoins; i++)
             {
---
+++ Assets/Scripts/OpenChest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class OpenChest : MonoBehaviour
+{
+    private float scaleSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3Int.one, Time.deltaTime * scaleSpeed);
+    }
+}
