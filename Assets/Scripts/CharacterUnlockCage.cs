2fb1c288a443de7a62ab5e6009599a35f2a62971
DutchRascal <DutchRascal@fritz.box>
2020-01-14 16:48:54 +0100

    DG-111-1

---
+++ Assets/Scripts/CharacterUnlockCage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUnlockCage : MonoBehaviour
+{
+    private bool canUnlock;
+
+    public GameObject message;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canUnlock = true;
+            message.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canUnlock = false;
+            message.SetActive(false);
+        }
+    }
+}
