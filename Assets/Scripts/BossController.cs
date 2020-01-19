0bd41133ae98b64c851a7638a5f242d3699f7b16
DutchRascal <DutchRascal@fritz.box>
2020-01-19 11:15:53 +0100

    DG-118

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+
+    public static BossController instance;
+
+    public BossAction[] actions;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+
+[System.Serializable]
+public class BossAction
+{
+    [Header("Action")]
+    public float actionLength;
+    public float moveSpeed;
+    public float timeBetweenShots;
+    public bool
+        shouldMove,
+        shouldChasePlayer,
+        moveToPoints,
+        shouldShoot;
+    public Transform pointToMoveTo;
+    public Transform[] shotPoints;
+    public GameObject itemToShoot;
+}
+
